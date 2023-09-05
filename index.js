4186a9ae48a706bd330ddac955003168314dfa59
Sahil <sahilismailnavar@gmail.com>
2023-09-05 16:33:02 +0530

    Modified the file made good looking .

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 let number = Math.floor((Math.random() * 100) + 1);
+//console.log(number);
 let chances = 0;
 let guess;
 
-console.log("This is a guessing game and you have to enter a number between 1 and 100.\n");
+console.log(" ---- This is a guessing game ---- \n\n\n You have to Enter a Number between 1 and 100.\n\n");
 
 do {
-  guess = prompt("Enter a number between 1 and 100");
+  guess = prompt("Enter a Number between 1 and 100 : ");
   guess = Number.parseInt(guess);
 
   if (isNaN(guess) || guess < 1 || guess > 100) {
-    console.log("Invalid input. Please enter a number between 1 and 100.");
+    console.log("Invalid input !!!!! \n Please Enter a Number between 1 and 100 ");
     continue;
   }
 
   chances++;
 
   if (guess > number) {
-    console.log("Number entered is greater.");
+    console.log("Number Entered is Greater ! Try Smaller \n");
   } else if (guess < number) {
-    console.log("Number entered is smaller.");
+    console.log("Number Entered is Smaller ! Try Greater \n");
   }
 
 } while (guess !== number);
 
-let score = 100 - chances;
-console.log("\nCongratulations 🥳🥳");
-console.log("The number generated was", number, "and you guessed it right! 😁");
-console.log("Your final score is", score);
+let score = chances;
+console.log("\n Congratulations 🥳🥳\n");
+console.log(" The Number generated was - ", number, "\n You guessed it Right!! 😁");
+console.log(" \n Your Final score is :- ", score);
